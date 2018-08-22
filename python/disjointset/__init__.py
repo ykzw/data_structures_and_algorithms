52c8a244303df6fa32e5e2bc50175d453699789f
ykzw <ysk.kozawa@gmail.com>
2018-08-22 17:17:58 +0900

    Add some description

--- python/disjointset/__init__.py
+++ python/disjointset/__init__.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Disjoint-set data structures"""
+"""Disjoint-set data structures
+
+Common interface:
+* Constructor()
+  - Create an empty structure
+
+* make_set(member)
+  - Make a set only consisting of "member"
+  - Assume "member" is not included in the sets
+
+* union(m1, m2)
+  - Merge two sets for "m1" and "m2"
+  - Return True if the sets are merged
+  - Return False otherwise (i.e., m1 and m2 are already in the same set)
+
+* find_set(member)
+  - Return a representative member of the set containing "member"
+
+"""
 
 from .linkedlist import LinkedListDisjointSets
 from .forest import DisjointSetForest
--- python/heap/__init__.py
+++ python/heap/__init__.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Heap data structures"""
+"""Heap data structures
+
+Common interface:
+* Constructor(A=None)
+  - Construct a heap from "A" if it's supplied
+  - Otherwise the heap is empty
+  - "A" is a sequence of key-id pairs
+
+* insert(key, id)
+  - Insert a pair of "key" and its "id" to the heap
+  - e.g., "key" is a priority and "id" is some ID
+
+* extract()
+  - Remove the pair with the minimum key from the heap and return it
+
+* decrease_key(id, newkey)
+  - Decrease the key of "id" to "newkey"
+  - Assume "newkey" is less than the current key
+
+* merge(other)
+  - Merge the "other" heap into the heap
+
+"""
 
 from .binary import BinaryHeap
 from .binomial import BinomialHeap
